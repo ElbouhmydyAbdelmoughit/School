6e689ac07177651e056945a94df71e1586838d45
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-04-27 10:05:37 +0000

    Maquette Dashboard page

--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/Normalize.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/sass/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2">
     <!-- <link rel="stylesheet" href="assets/css/animate.min.css"> -->
     <title>School</title>
